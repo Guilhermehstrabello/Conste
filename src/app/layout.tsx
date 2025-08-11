cc9a1278b9bdbaabdb542f6de3187c1e418ca281
Guilhermehstrabello <gui.strabehonorio@outlook.com>
2025-08-11 14:31:27 -0300

    ajuste na clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,15 +28,19 @@ export default function RootLayout({
         />
       </head>
       <body className="font-montserrat">
-          <Script id="microsoft-clarity-analytics" strategy="lazyOnload">
-                  {`
-                    (function(c,l,a,r,i,t,y){
-                      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                    })(window, document, "clarity", "script", "[sjxf5yw5cb]");
-                  `}
-                </Script>
+          {/* Script Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "sjxf5yw5cb");
+          `}
+        </Script>
         <Analytics />
         <GoogleTagManager gtmId="GTM-TVRWH47F" />
         {children}
