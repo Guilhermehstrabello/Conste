4ceea5cb782aba78558d5ff745c2613d7ccca4b0
Guilherme Strabello <gui.strabehonorio@outlook.com>
2026-03-26 15:23:05 -0300

    #

--- src/components/clientslogo.tsx
+++ src/components/clientslogo.tsx
@@ -17,7 +17,7 @@ export default function ClientsLogo() {
       </ul>
 
       <ul
-        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+        className="flex items-center justify-center md:justify-start [&_li]:mx-14 [&_img]:max-w-none animate-infinite-scroll"
         aria-hidden="true"
       >
         {logos.map((logo) => (
